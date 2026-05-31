c09f8c8c99ffbb9c3db8f2f4e9f6a3327b462a4b
Mikhail <s34040@pjwstk.edu.pl>
2026-05-31 23:10:26 +0200

    Fix missing kDebugMode import in intro_lane_loader.dart

--- lib/lane/pages/intro_lane_loader.dart
+++ lib/lane/pages/intro_lane_loader.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
